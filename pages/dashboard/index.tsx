edfcaef90f2a665c274fb2ed002606fe58fe24fb
Ipsita Bhattacharyya <queenipsita66@gmail.com>
2022-12-26 19:03:11 +0530

    dashboard changes

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,39 +1,48 @@
 import getDashboardPageCollection from "../../backend/dasboardCollectionAPI";
 import DashboardPage from "../../components/organisms/DashboardPage";
 import SignInPage from "../../components/organisms/SignInPage";
-import { withSessionSsr } from "../../lib/withSession";
+// import { withSessionSsr } from "../../lib/withSession";
 
-export const getServerSideProps = withSessionSsr(
-    async function getServerSideProps({ req }) {
-        // console.log("coming in getServerSideProps");
-        // const user = req.session?.user;
-        // console.log("user - ", user);
+// export const getServerSideProps = withSessionSsr(
+//     async function getServerSideProps({ req }) {
+//         // console.log("coming in getServerSideProps");
+//         // const user = req.session?.user;
+//         // console.log("user - ", user);
 
-        // if (user && user?.email) {
-        //     const dashboardData = await getDashboardPageCollection();
-        //     return {
-        //         props: {
-        //             authenticated: true,
-        //             dashboardData: dashboardData
-        //         },
-        //     };
-        // }
-        const dashboardData = await getDashboardPageCollection();
-        return {
-            props: {
-                authenticated: true,
-                dashboardData: dashboardData
-            },
-        };
-    },
-);
+//         // if (user && user?.email) {
+//         //     const dashboardData = await getDashboardPageCollection();
+//         //     return {
+//         //         props: {
+//         //             authenticated: true,
+//         //             dashboardData: dashboardData
+//         //         },
+//         //     };
+//         // }
+//         const dashboardData = await getDashboardPageCollection();
+//         return {
+//             props: {
+//                 authenticated: true,
+//                 dashboardData: dashboardData
+//             },
+//         };
+//     },
+// );
+
+export async function getStaticProps() {
+    const dashboardData = await getDashboardPageCollection();
+    return {
+        props: {
+            dashboardData: dashboardData
+        },
+    };
+}
 
 const Dashboard = (props: any) => {
-    if (props.authenticated) {
-        return <DashboardPage {...props} />
-    } else {
-        return <SignInPage {...props}/>
-    }
+    // if (props.authenticated) {
+    return <DashboardPage {...props} />
+    // } else {
+    //     return <SignInPage {...props} />
+    // }
 }
 
 export default Dashboard;
